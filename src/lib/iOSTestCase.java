fcef65a7f1d9859eb420a45f62a6001227a1c99b
llien3015 <129271330+llien3015@users.noreply.github.com>
2023-05-30 00:02:05 +0400

    Add files via upload

---
+++ src/lib/iOSTestCase.java
@@ -0,0 +1,54 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.time.Duration;
+
+public class iOSTestCase extends TestCase {
+
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723";
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone 14");
+        capabilities.setCapability("platformVersion", "16.4");
+        capabilities.setCapability("app", "/Users/admin/Desktop/JavaAppiumAutomation iOS/JavaAppiumAutomation2/apks/Wikipedia690.app");
+        capabilities.setCapability("appium:automationName", "XCUITest");
+
+        driver = new IOSDriver(new URL(AppiumURL), capabilities);
+        this.rotateScreenPortrate();
+    }
+
+    @Override
+    protected void tearDown() throws Exception
+    {
+        driver.quit();
+        super.tearDown();
+    }
+    protected void rotateScreenPortrate()
+    {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    protected void rotateScreenLandscape()
+    {
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+     protected void backgroundApp(int seconds)
+     {
+         driver.runAppInBackground(Duration.ofSeconds(seconds));
+     }
+}
